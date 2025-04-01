dc203d26ece9f49a765dffe03a706d7f57178ac6
alexmmq <alexmmqb@gmail.com>
2025-04-01 20:27:01 +0300

    added Worker class definition

--- src/CustomThreadPool/MyThreadPoolExecutor.java
+++ src/CustomThreadPool/MyThreadPoolExecutor.java
@@ -4,8 +4,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+import jdk.internal.vm.*;
 
 public class MyThreadPoolExecutor implements CustomExecutor{
 
@@ -121,6 +123,144 @@ public class MyThreadPoolExecutor implements CustomExecutor{
     private final Condition termination = mainLock.newCondition();
 
 
+    /**
+     * Класс, в основе содержащий сет для хранения потоков и предоставляющий удобный инструментарий при инициализации
+     * пула потоков
+     */
+    private final SharedThreadContainer container;
+
+    /**
+     * Трекер максимального размера пула. Доступ при получении mainLock
+     */
+    private int largestPoolSize;
+
+    /**
+     * Счетчик завершенных задач. Обновление счетчика происходит при завершении рабочих потоков. Доступ при получении mainLock
+     */
+    private long completedTaskCount;
+
+    /**
+     *Factory для создания новых потоков, в нашем случае необходимо переопределить реализацию ThreadFactory
+     * с возможностью логирования процесса создания потоков.
+     */
+    private volatile ThreadFactory threadFactory;
+
+    /**
+     * Способ обработки отказа, необходимо определить что делать с новой поступившей задачей
+     */
+    private volatile RejectedExecutionHandler handler;
+
+    /**
+     * При значении false базовые потоки остаются живыми. При значении true базовые потоки будут завершены по истечении
+     * времени, переданного через keepAliveTime
+     */
+
+    private volatile boolean allowCoreThreadTimeOut;
+
+    /*
+    Присваиваем значение по умолчанию для применяемой политики отказа
+     */
+    private static final RejectedExecutionHandler defaultHandler = null;
+
+    /**
+     * Необходимое снятие ограничений для вызовов shutdown() и shutdownNow() - в случае инициации которых происходит
+     * прерывание потока
+     */
+    private static final RuntimePermission shutdownPerm =
+            new RuntimePermission("modifyThread");
+
+    /**
+     * Class Worker mainly maintains interrupt control state for
+     * threads running tasks, along with other minor bookkeeping.
+     * This class opportunistically extends AbstractQueuedSynchronizer
+     * to simplify acquiring and releasing a lock surrounding each
+     * task execution.  This protects against interrupts tha0t are
+     * intended to wake up a worker thread waiting for a task from
+     * instead interrupting a task being run.  We implement a simple
+     * non-reentrant mutual exclusion lock rather than use
+     * ReentrantLock because we do not want worker tasks to be able to
+     * reacquire the lock when they invoke pool control methods like
+     * setCorePoolSize.  Additionally, to suppress interrupts until
+     * the thread actually starts running tasks, we initialize lock
+     * state to a negative value, and clear it upon start (in
+     * runWorker).
+     */
+    private final class Worker
+            extends AbstractQueuedSynchronizer
+            implements Runnable
+    {
+        /**
+         * This class will never be serialized, but we provide a
+         * serialVersionUID to suppress a javac warning.
+         */
+        private static final long serialVersionUID = 6138294804551838833L;
+
+        /** Thread this worker is running in.  Null if factory fails. */
+        @SuppressWarnings("serial") // Unlikely to be serializable
+        final Thread thread;
+        /** Initial task to run.  Possibly null. */
+        @SuppressWarnings("serial") // Not statically typed as Serializable
+                Runnable firstTask;
+        /** Per-thread task counter */
+        volatile long completedTasks;
+
+        // TODO: switch to AbstractQueuedLongSynchronizer and move
+        // completedTasks into the lock word.
+
+        /**
+         * Creates with given first task and thread from ThreadFactory.
+         * @param firstTask the first task (null if none)
+         */
+        Worker(Runnable firstTask) {
+            setState(-1); // inhibit interrupts until runWorker
+            this.firstTask = firstTask;
+            this.thread = getThreadFactory().newThread(this);
+        }
+
+        /** Delegates main run loop to outer runWorker. */
+        public void run() {
+            runWorker(this);
+        }
+
+        // Lock methods
+        //
+        // The value 0 represents the unlocked state.
+        // The value 1 represents the locked state.
+
+        protected boolean isHeldExclusively() {
+            return getState() != 0;
+        }
+
+        protected boolean tryAcquire(int unused) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        protected boolean tryRelease(int unused) {
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        public void lock()        { acquire(1); }
+        public boolean tryLock()  { return tryAcquire(1); }
+        public void unlock()      { release(1); }
+        public boolean isLocked() { return isHeldExclusively(); }
+
+        void interruptIfStarted() {
+            Thread t;
+            if (getState() >= 0 && (t = thread) != null && !t.isInterrupted()) {
+                try {
+                    t.interrupt();
+                } catch (SecurityException ignore) {
+                }
+            }
+        }
+    }
+
     public MyThreadPoolExecutor(int corePoolSize,
                                 int maxPoolSize,
                                 long keepAliveTime, TimeUnit unit,
@@ -131,6 +271,7 @@ public class MyThreadPoolExecutor implements CustomExecutor{
         this.minSpareThreads = minSpareThreads;
         this.queueSize = queueSize;
         this.workQueue = workQueue;
+        container = null;
     }
 
     @Override
