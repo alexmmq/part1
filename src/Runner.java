bc2a3d728b73e24694183e94a3198aeeb8d2b5a3
alexmmq <alexmmqb@gmail.com>
2025-04-14 12:21:29 +0300

    v.0.9.9

--- src/Runner.java
+++ src/Runner.java
@@ -8,48 +8,43 @@ public class Runner {
     private static final Logger LOGGER = Logger.getLogger(Runner.class.getName());
 
     public static void main(String[] args) throws InterruptedException {
-        // Инициализация пула потоков с заданными параметрами
         CustomThreadPoolExecutor executor = new CustomThreadPoolExecutor(
-                2,          // corePoolSize
-                4,          // maxPoolSize
-                5,          // keepAliveTime в секундах
-                TimeUnit.SECONDS, // единица измерения времени
-                5,          // queueSize
-                1           // minSpareThreads
-        );
+                2,
+                4,
+                5,
+                TimeUnit.SECONDS,
+                5,
+                1);
 
-        // Создание имитационной задачи
+
         Runnable imitationTask = () -> {
             try {
-                // Имитация выполнения задачи
-                Thread.sleep(2000); // Задержка на 2 секунды
+                Thread.sleep(2000);
                 LOGGER.info("Task started: " + Thread.currentThread().getName());
-                Thread.sleep(3000); // Дополнительная задержка на 3 секунды
+                Thread.sleep(3000);
                 LOGGER.info("Task finished: " + Thread.currentThread().getName());
             } catch (InterruptedException e) {
                 LOGGER.severe("Task interrupted: " + e.getMessage());
             }
         };
 
-        // Отправка нескольких задач в пул
+
         for (int i = 0; i < 5; i++) {
             executor.execute(imitationTask);
         }
 
-        // Даем немного времени на выполнение задач
+
         Thread.sleep(15000);
 
-        // Вызов shutdown(), чтобы завершить пул потоков
         executor.shutdown();
 
-        // Проверка, что все задачи завершились
         LOGGER.info("Waiting for all tasks to finish...");
         executor.awaitTermination(30, TimeUnit.SECONDS);
         LOGGER.info("All tasks have been completed.");
 
-        // Демонстрация отклонения задач, если пул закрыт
         Runnable rejectedTask = () -> LOGGER.info("Rejected task attempted to be executed.");
-        executor.execute(rejectedTask); // Эта задача будет отклонена, так как пул закрыт
+        executor.execute(rejectedTask);
+        // Эта задача будет отклонена, так как пул закрыт, выбросит Exception
 
         LOGGER.info("Testing rejection of tasks after shutdown.");
     }
